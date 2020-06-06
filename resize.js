4129ef829b7ad05e1e46b36ce89603da47b25158
namesofkim <30866302+imhjk@users.noreply.github.com>
2020-06-06 10:17:30 +0200

    Initial commit

---
+++ resize.js
@@ -0,0 +1,11 @@
+$("div#section2").on('click', function(){
+	$("div#section2").toggleClass('bigger');
+  $("div#section1").toggleClass('smaller');
+});
+
+
+
+
+$("div#about").on('click', function(){
+	$("div#about2").toggleClass('open');
+});
